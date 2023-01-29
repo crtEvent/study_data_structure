82ca052586f96a04c221179783026a44f1af8d54
crtEvent <crtevn@gmail.com>
2023-01-29 23:46:44 +0900

    Add(MyBinaryTree): MyBinaryTree 뼈대 작성

---
+++ ds/non_linear/tree/binarytree/MyBinaryTree.java
@@ -0,0 +1,41 @@
+package ds.non_linear.tree.binarytree;
+
+public class MyBinaryTree {
+
+    private TreeNode rootNode;
+    private int count = 0;
+
+    public class TreeNode {
+        Object data;
+        TreeNode leftNode;
+        TreeNode rightNode;
+
+        public TreeNode(Object data, TreeNode leftNode, TreeNode rightNode) {
+            this.data = data;
+            this.leftNode = leftNode;
+            this.rightNode = rightNode;
+        }
+    }
+
+    public void makeBinaryTree() {
+
+    }
+
+    public void addLeft() {
+
+    }
+
+    public void addRight() {
+
+    }
+
+    public void preOrder() {}
+
+    public void inOrder() {}
+
+    public void postOrder() {}
+
+
+
+
+}
