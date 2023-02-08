61b0c896bdd7d072be43295415ff2b302e972896
crtEvent <crtevn@gmail.com>
2023-02-08 16:03:23 +0900

    Add(MyBinaryTree): preOrderTraversal() 전위 순회 추가

--- ds/non_linear/tree/binarytree/MyBinaryTree.java
+++ ds/non_linear/tree/binarytree/MyBinaryTree.java
@@ -1,9 +1,12 @@
 package ds.non_linear.tree.binarytree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyBinaryTree {
 
     private TreeNode rootNode;
-    private int count = 0;
+    private int count = 100;
 
     public class TreeNode {
         Object data;
@@ -21,6 +24,13 @@ public class MyBinaryTree {
         }
     }
 
+    /**
+     * 서브 바이나리 트리 생성
+     * @param left 왼쪽 자식 노드
+     * @param data 데이터
+     * @param right 오른쪽 자식 노드
+     * @return
+     */
     public TreeNode makeSubBinaryTree(TreeNode left, Object data, TreeNode right) {
         TreeNode root = new TreeNode(data);
         root.leftNode = left;
@@ -29,15 +39,30 @@ public class MyBinaryTree {
         return root;
     }
 
-    public void addLeft() {
+    /**
+     * 전위 순회(Preorder Traversal)
+     * 현재 노드 -> 왼쪽 노드 -> 오른쪽 노드 순으로 순회
+     * @param rootNode
+     * @return
+     */
+    public List<Object> preOrderTraversal(TreeNode rootNode) {
+        List<Object> dataGroup = new ArrayList<>(count);
+        if(rootNode != null) {
+            dataGroup.add(rootNode.data);
+            preOrderTraversal(rootNode.leftNode, dataGroup);
+            preOrderTraversal(rootNode.rightNode, dataGroup);
+        }
 
+        return dataGroup;
     }
 
-    public void addRight() {
-
+    private void preOrderTraversal(TreeNode rootNode, List<Object> dataGroup) {
+        if(rootNode != null) {
+            dataGroup.add(rootNode.data);
+            preOrderTraversal(rootNode.leftNode, dataGroup);
+            preOrderTraversal(rootNode.rightNode, dataGroup);
+        }
     }
-
-    public void preOrder() {}
 
     public void inOrder() {}
 
---
+++ ds/non_linear/tree/binarytree/MyBinaryTreeTest.java
@@ -0,0 +1,49 @@
+package ds.non_linear.tree.binarytree;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Objects;
+
+public class MyBinaryTreeTest {
+
+    MyBinaryTree tree;
+    MyBinaryTree.TreeNode root;
+
+    @BeforeEach
+    public void makeBinaryTree() {
+        /* [Tree Structure]
+         *      (A)
+         *     /   \
+         *  (B)     (C)
+         *  /  \    /  \
+         * (D) (E) (F) (G)
+         */
+        /* [Tree Structure]
+         *      (1)
+         *     /   \
+         *  (2)     (3)
+         *  /  \    /  \
+         * (4) (5) (6) (7)
+         */
+        tree = new MyBinaryTree();
+        MyBinaryTree.TreeNode n7 = tree.makeSubBinaryTree(null, "G", null);
+        MyBinaryTree.TreeNode n6 = tree.makeSubBinaryTree(null, "F", null);
+        MyBinaryTree.TreeNode n5 = tree.makeSubBinaryTree(null, "E", null);
+        MyBinaryTree.TreeNode n4 = tree.makeSubBinaryTree(null, "D", null);
+        MyBinaryTree.TreeNode n3 = tree.makeSubBinaryTree(n6, "C", n7);
+        MyBinaryTree.TreeNode n2 = tree.makeSubBinaryTree(n4, "B", n5);
+        root = tree.makeSubBinaryTree(n2, "A", n3);
+    }
+
+    @Test
+    public void testPreOrder() {
+        List<Object> dataGroup = tree.preOrderTraversal(root);
+
+        dataGroup.stream()
+                .filter(Objects::nonNull)
+                .map(obj -> String.valueOf(obj))
+                .forEach(System.out::println);
+    }
+}
