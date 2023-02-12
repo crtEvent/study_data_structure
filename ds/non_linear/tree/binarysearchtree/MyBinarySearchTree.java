088e1cf8dd48b5168c05aaf3ef46e8b19f12baec
crtEvent <crtevn@gmail.com>
2023-02-12 16:10:03 +0900

    Add(MyBinarySearchTree): MyBinarySearchTree 추가

---
+++ ds/non_linear/tree/binarysearchtree/MyBinarySearchTree.java
@@ -0,0 +1,16 @@
+package ds.non_linear.tree.binarysearchtree;
+
+public class MyBinarySearchTree {
+
+    private BST_Node root = new BST_Node();
+
+    public class BST_Node {
+        Object data;
+        BST_Node leftNode;
+        BST_Node rightNode;
+
+        public BST_Node() {
+
+        }
+    }
+}
