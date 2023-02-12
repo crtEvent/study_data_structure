b4a7317c7488c660882b6aa36ebd635b0dd56d1c
crtEvent <crtevn@gmail.com>
2023-02-12 16:33:28 +0900

    Add(MyBinarySearchTree): BST_Node 생성자 추가

--- ds/non_linear/tree/binarysearchtree/MyBinarySearchTree.java
+++ ds/non_linear/tree/binarysearchtree/MyBinarySearchTree.java
@@ -2,15 +2,25 @@ package ds.non_linear.tree.binarysearchtree;
 
 public class MyBinarySearchTree {
 
-    private BST_Node root = new BST_Node();
+    private BST_Node root;
 
     public class BST_Node {
+        int key;
         Object data;
         BST_Node leftNode;
         BST_Node rightNode;
 
-        public BST_Node() {
+        public BST_Node(int key, Object data) {
+            this(key, data, null, null);
+        }
 
+        public BST_Node(int key, Object data, BST_Node leftNode, BST_Node rightNode) {
+            this.key = key;
+            this.data = data;
+            this.leftNode = leftNode;
+            this.rightNode = rightNode;
         }
     }
+
+
 }
