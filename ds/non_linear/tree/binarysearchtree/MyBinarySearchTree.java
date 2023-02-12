c2dcecc0f9370b26e80d98ed6530fa97ca3b2845
crtEvent <crtevn@gmail.com>
2023-02-12 17:48:38 +0900

    Add(MyBinarySearchTree): insert() 추가

--- ds/non_linear/tree/binarysearchtree/MyBinarySearchTree.java
+++ ds/non_linear/tree/binarysearchtree/MyBinarySearchTree.java
@@ -22,5 +22,23 @@ public class MyBinarySearchTree {
         }
     }
 
+    private BST_Node insert(BST_Node root, int key, Object data) {
+        BST_Node newNode = new BST_Node(key, data);
 
+        if(root == null) {
+            return newNode;
+        } else if(newNode.key < root.key) {
+            root.leftNode = insert(root.leftNode, key, data);
+            return root;
+        } else if(newNode.key > root.key) {
+            root.rightNode = insert(root.rightNode, key, data);
+            return root;
+        }
+
+        return root;
+    }
+
+    public void insert(int key, Object data) {
+        root = insert(root, key, data);
+    }
 }
